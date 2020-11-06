f18cccc5b4ac89957cb6b1d6bef3f0636487f2ec
levyofun <58773852+levyofun@users.noreply.github.com>
2020-11-07 00:48:21 +0700

    added movement v 0.1

---
+++ Assets/Scripts/CharacterCtrl2D.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCtrl2D : MonoBehaviour
+{
+	[Range (0,1f)] public float m_MovementSmoothing = 0.5f;
+	public Rigidbody2D m_Rigidbody2D;
+	private Vector3 m_Velocity = Vector3.zero;
+
+	public void Move(float move)
+	{
+		// Move the character by finding the target velocity
+		Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+		// And then smoothing it out and applying it to the character
+		m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal") * speed;
+        movement.y = Input.GetAxis("Vertical") * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+    }
+	
+}
