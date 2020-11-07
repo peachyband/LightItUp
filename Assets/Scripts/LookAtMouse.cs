4e1565875152329acd698646c1963ef7092835c8
levyofun <58773852+levyofun@users.noreply.github.com>
2020-11-07 21:09:10 +0700

    some bugs

---
+++ Assets/Scripts/LookAtMouse.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour
+{
+    private void LateUpdate()
+    {
+        // get the mouse position in world space
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // change mouse Z position equal to this object's
+        mousePos.z = transform.position.z;
+
+        // get a rotation that points Z axis forward, and the Y axis towards the target
+        Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, (mousePos - transform.position));
+
+        // apply new rotation
+        transform.rotation = targetRotation;
+
+        // rotate 90 degrees around the Z axis to point X axis instead of Y
+        transform.Rotate(0, 0, 90);
+    }
+}
